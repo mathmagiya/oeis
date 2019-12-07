df6cfb2c6025c92ed5a407041df0c5810701b707
mathmagiya <58531241+mathmagiya@users.noreply.github.com>
2019-12-07 23:02:56 +0200

    Create A327142.cpp
    a(n) is the number of different sizes of integer-sided rectangles which can be placed inside an n X n square and with length greater than n.
    
    The first 30 terms are:
    0, 0, 0, 0, 1, 1, 1, 3, 3, 6, 6, 6, 10, 10, 15, 15, 21, 21, 21, 28, 28, 36, 36, 36, 45, 45, 55, 55, 66, 66,...
    
    http://oeis.org/A327142

---
+++ A327142.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+int main() {
+int n;
+cin>>n;
+vector <int> v;
+for (int i=1; i<=n; i++)
+{int count=0;
+// ii-length, jj-width of diagonal rectangle
+for (int ii=i+1; ii<=int(sqrt(2)*i); ii++)
+     for (int jj=1; jj<=i; jj++)
+          if ((double(i)-double(ii)/sqrt(2))-double(jj)/sqrt(2)>0)
+                    {count++; }
+v.push_back(count);
+}
+for (int i=0; i<v.size(); i++)
+cout << v[i] << ", ";
+return 0;
+}
